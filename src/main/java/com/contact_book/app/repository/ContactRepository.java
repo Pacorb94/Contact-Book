51a7e6cf32f669c6517f96a5472e6da6b15a4358
Pacorb94 <a@a.com>
2022-01-30 18:50:13 +0100

    ADD ContactRepository

--- src/main/java/com/contact_book/app/repository/ContactRepository.java
+++ src/main/java/com/contact_book/app/repository/ContactRepository.java
@@ -1,19 +1,21 @@
-package com.contact_book.app.dao;
+package com.contact_book.app.repository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.contact_book.app.models.Contact;
+import com.contact_book.app.model.Contact;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-	
-	
-	//Al usar findBy... ya está configurado para buscar
-	Contact findByEmail(String email);
-	
+
+	@Transactional(readOnly = true)
+	// Al usar findBy... ya está configurado para buscar
+	Optional <Contact> findByEmail(String email);
+
 	@Transactional
 	@Query("delete from contacts co where co.email=?1")
 	void deleteByEmail(String email);
