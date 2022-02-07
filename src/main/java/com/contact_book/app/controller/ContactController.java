520577a45782514c20fc535d1be27f94b31da319
Pacorb94 <a@a.com>
2022-02-07 22:59:16 +0100

    CHANGE ContactController

--- src/main/java/com/contact_book/app/controller/ContactController.java
+++ src/main/java/com/contact_book/app/controller/ContactController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import com.contact_book.app.exceptions.EmailAlreadyExistsException;
 import com.contact_book.app.exceptions.ModelNotFoundException;
 import com.contact_book.app.model.Contact;
+import com.contact_book.app.request.ContactRequest;
 import com.contact_book.app.service.ContactService;
-
 
 @RestController
 @RequestMapping("/contacts")
@@ -23,10 +23,11 @@ public class ContactController {
 	private ContactService contactService;
 
 	@PostMapping
-	public ResponseEntity<Contact> create(@Valid @RequestBody Contact request) {
-		Contact contact=this.contactService.getContactByEmail(request.getEmail());
-		if(contact == null) {	
-			return ResponseEntity.created(null).body(this.contactService.save(request));		
+	public ResponseEntity<Contact> create(@Valid @RequestBody ContactRequest request) {
+		if(this.contactService.getContactByEmail(request.getEmail()) == null) {	
+			Contact contact=new Contact();
+			contact.setAllProperties(request);
+			return ResponseEntity.created(null).body(this.contactService.save(contact));		
 		}
 		throw new EmailAlreadyExistsException("Email already exists");
 	}
@@ -42,17 +43,26 @@ public class ContactController {
 		if (contact != null) {
 			return ResponseEntity.ok(contact);
 		}
-		throw new ModelNotFoundException("Model not found");
+		throw new ModelNotFoundException("Contact not found");
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Contact> update(@Valid @RequestBody Contact request, @PathVariable Long id) {
+	public ResponseEntity<Contact> update(
+			@Valid @RequestBody ContactRequest request, 
+			@PathVariable Long id
+	) {
 		Contact contact = this.contactService.getContact(id);
 		if (contact != null) {
+			Contact contactAux=this.contactService.getContactByEmail(request.getEmail());
+			/*Comprobamos si el email ya existe y si el id es distinto al que 
+			vamos a modificar*/
+			if(contactAux != null && contactAux.getId() != id) {
+				throw new EmailAlreadyExistsException("Email already exists");
+			}
 			BeanUtils.copyProperties(request, contact);
 			return ResponseEntity.ok(this.contactService.save(contact));
 		}
-		throw new ModelNotFoundException("Model not found");
+		throw new ModelNotFoundException("Contact not found");
 	}
 
 	@DeleteMapping("/{id}")
@@ -62,6 +72,6 @@ public class ContactController {
 			this.contactService.delete(id);
 			return ResponseEntity.noContent().build();
 		}
-		throw new ModelNotFoundException("Model not found");
+		throw new ModelNotFoundException("Contact not found");
 	}
 }
